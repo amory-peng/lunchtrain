f2a1935664171f105192b6901bae315f8e60c2d9
Amory Peng <amoryp@amazon.com>
2018-10-02 14:30:32 -0700

    finished poll

--- bot.js
+++ bot.js
@@ -3,14 +3,15 @@ const auth = require('./auth.json');
 const LunchTrain = require('./lib/lunch_train').default;
 
 const client = new Discord.Client();
+let train = null;
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', (message) => {
-  if (message.content === '!start') {
-    const train = new LunchTrain(message);
+  if (message.content === '!start' && (!train || train.hasLeft())) {
+    train = new LunchTrain(message);
     train.start();
   }
 });
