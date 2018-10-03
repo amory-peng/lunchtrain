abc6a6fad9bd65630ad68aeeefd17e9ef8c4737d
Amory Peng <amoryp@amazon.com>
2018-10-03 16:06:14 -0700

    better channel handling

--- bot.js
+++ bot.js
@@ -3,15 +3,19 @@ const auth = require('./auth.json');
 const LunchTrain = require('./lib/lunch_train').default;
 
 const client = new Discord.Client();
-let train = null;
+const trains = {};
 
 client.on('ready', () => {
   console.log(`Bot started at: ${new Date().toString()}`);
 });
 
 client.on('message', (message) => {
-  if (message.content === '!start' && (!train || train.hasLeft())) {
+  if (message.content !== '!start') return;
+  let train = trains[message.channel];
+  if (!train || train.hasLeft()) {
+    console.log(`train added for ${message.channel}`);
     train = new LunchTrain(message);
+    trains[message.channel] = train;
     train.start();
   }
 });
